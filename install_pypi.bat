fff698e6a8dcbae888eadf45bc7067c047dc9ffb
admin <admin>
2015-12-15 17:19:27 +0530

    modified git changes

--- install_pypi.bat
+++ install_pypi.bat
@@ -1,3 +1,5 @@
-if not exist c:\continuous_integration git clone https://github.com/racoextrix/continuous_integration.git 
+if not exist c:\continuous_integration git clone https://github.com/racoextrix/continuous_integration.git
+cd c:\continuous_integration
+if exist c:\continuous_integration git pull origin master
 %WORKON_HOME%\%1\Scripts\easy_install c:\continuous_integration\mypypi\MySQL_python-1.2.5-py2.7-win32.egg
 %WORKON_HOME%\%1\Scripts\pip install --no-index --find-links=c:\continuous_integration\mypypi -r c:\continuous_integration\requirements.txt
